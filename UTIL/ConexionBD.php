c732c1c31497c88be16a4ff066e1f043836989a5
Manuel Alcalde <mac2291998@gmail.com>
2018-10-02 17:16:53 -0500

    update databse 2

--- UTIL/ConexionBD.php
+++ UTIL/ConexionBD.php
@@ -1,17 +1,15 @@
-<?php class ConexionBD {
-    const SERVER="pda.vrammdev.com";
-    const USER="giampieer24";
-    const PASS="Gi@mpieer24-pe";
-    const DATABASE="pda";
-    private  $cn=null;
-    
-    public function getconecionBD() {
-        try {
-            $this->cn= mysqli_connect(self::SERVER,self::USER, self::PASS, self::DATABASE,self::PORT);
-        } catch (Exception $exc) {
-          
-        }
-        return $this->cn;
-        }
+<?php
+class ConexionBD {
+  const SERVER = "localhost";
+  const USER = "root";
+  const PASS = "Gi@mpieer24-pe";
+  const DATABASE = "pda";
+  private  $cn = null;
+  public function getconecionBD() {
+    try {
+      $this->cn = mysqli_connect(self::SERVER,self::USER, self::PASS, self::DATABASE);
+      } catch (Exception $exc) {
+      }
+      return $this->cn;
+  }
 }
-?>
